f0c142b17f0d674732f0e371f9341314ec6168bb
Toshiyuki HINA <thina@yourinventit.com>
2016-04-21 10:56:27 +0900

    Add components in material-ui usage

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
+import MyAwesomeReactComponent from './my_awesome_react_component';
 
 export default class App extends React.Component {
 
   render() {
-    return <div>App</div>;
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <MyAwesomeReactComponent />
+      </MuiThemeProvider>
+    );
   }
   
 }
---
+++ app/assets/javascripts/components/my_awesome_react_component.js.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button'
+
+export default class MyAwesomeReactComponent extends React.Component {
+
+  render() {
+    return (
+      <RaisedButton label="Default" />
+    );
+  }
+  
+}
